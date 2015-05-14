1021d310dee4c16005c324668ee11f6f08cca91e
Katrina Owen <katrina.owen@gmail.com>
2015-05-13 17:18:09 -0700

    Recurse in extract aggregates if more than one level deep

--- lib/coreaggregate_test.go
+++ lib/coreaggregate_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestExtractAggregates(t *testing.T) {
-	s := `
+	agg1 := `
 	{
 		"instruments": {
 			"doc_count_error_upper_bound": 0,
@@ -52,35 +52,75 @@ func TestExtractAggregates(t *testing.T) {
 	}
 	`
 
-	sr := &SearchResult{Aggregations: []byte(s)}
-
-	buckets, err := ExtractAggregates(sr)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(buckets) != 2 {
-		t.Fatalf("got %d aggregates, expected 2", len(buckets))
-	}
-
-	for _, bucket := range buckets {
-		switch bucket.Name {
-		case "tags":
-			if len(bucket.KeyCount) == 3 {
-				assert.Equal(t, 2, bucket.KeyCount["drum"])
-				assert.Equal(t, 3, bucket.KeyCount["house"])
-				assert.Equal(t, 1, bucket.KeyCount["bass"])
-			} else {
-				t.Errorf("got %d keys in 'tags' bucket, expected 3", len(bucket.KeyCount))
+	agg2 := `{
+		"people": {
+			"doc_count": 5,
+			"friends": {
+				"doc_count_error_upper_bound": 0,
+				"sum_other_doc_count": 0,
+				"buckets": [
+					{
+						"key": "alice",
+						"doc_count": 2
+					},
+					{
+						"key": "bob",
+						"doc_count": 1
+					}
+				]
 			}
-		case "instruments":
-			if len(bucket.KeyCount) == 4 {
-				assert.Equal(t, 4, bucket.KeyCount["drums"])
-				assert.Equal(t, 1, bucket.KeyCount["cello"])
-				assert.Equal(t, 3, bucket.KeyCount["violin"])
-				assert.Equal(t, 2, bucket.KeyCount["bongo"])
-			} else {
-				t.Errorf("got %d keys in 'instruments' bucket, expected 4", len(bucket.KeyCount))
+		}
+	}`
+
+	testCases := []struct {
+		agg         string
+		bucketCount int
+	}{
+		{agg1, 2},
+		{agg2, 1},
+	}
+	for i, tc := range testCases {
+
+		sr := &SearchResult{Aggregations: []byte(tc.agg)}
+
+		buckets, err := ExtractAggregates(sr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(buckets) != tc.bucketCount {
+			t.Errorf("Expected case %d to have a bucket count of %d, but got %d", i, tc.bucketCount, len(buckets))
+			continue
+		}
+
+		for _, bucket := range buckets {
+			switch bucket.Name {
+			case "tags":
+				if len(bucket.KeyCount) == 3 {
+					assert.Equal(t, 2, bucket.KeyCount["drum"])
+					assert.Equal(t, 3, bucket.KeyCount["house"])
+					assert.Equal(t, 1, bucket.KeyCount["bass"])
+				} else {
+					t.Errorf("got %d keys in 'tags' bucket, expected 3", len(bucket.KeyCount))
+				}
+			case "instruments":
+				if len(bucket.KeyCount) == 4 {
+					assert.Equal(t, 4, bucket.KeyCount["drums"])
+					assert.Equal(t, 1, bucket.KeyCount["cello"])
+					assert.Equal(t, 3, bucket.KeyCount["violin"])
+					assert.Equal(t, 2, bucket.KeyCount["bongo"])
+				} else {
+					t.Errorf("got %d keys in 'instruments' bucket, expected 4", len(bucket.KeyCount))
+				}
+			case "friends":
+				if len(bucket.KeyCount) == 2 {
+					assert.Equal(t, 2, bucket.KeyCount["alice"])
+					assert.Equal(t, 1, bucket.KeyCount["bob"])
+				} else {
+					t.Errorf("got %d keys in 'friends' bucket, expected 2", len(bucket.KeyCount))
+				}
+			default:
+				t.Errorf("case %d: Unexpected key %s", i, bucket.Name)
 			}
 		}
 	}
